2b09e4630c0c51bd47bf5dcd5331d3e338309489
yuki lin <christinelinyunyun8@gmail.com>
2022-10-15 08:04:39 +0800

    done HW4

--- HW4/main.py
+++ HW4/main.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
 class Kernel:
     def __init__(self, array, origin):
@@ -33,7 +34,7 @@ def binary_dilation(image, kernel): #input should be binary image
     # scan each pixel in image
     for i in range(row):
         for j in range(col):
-            if(image[i, j] == 1): # if image pixel is white
+            if(image[i, j] != 0): # if image pixel is white
                 # scan each pixel in kernel
                 for k in range(0, kernel.row):
                     for l in range(0, kernel.col):
@@ -46,56 +47,66 @@ def binary_dilation(image, kernel): #input should be binary image
 
     return result
 
-
 def binary_erosion(image, kernel): #input should be binary image
     result = np.zeros(image.shape)
     row, col = image.shape
     # scan each pixel in image
     for i in range(row):
         for j in range(col):
-            if(image[i, j] == 1): # if image pixel is white
-                # print(f'\nimg[{i}, {j}]')
-                fit = True
-                # scan each pixel in kernel
-                k = 0
-                l = 0
-                while(fit and k < kernel.row):
-                    while(l < kernel.col):
-                        # print(f'kernel [{k}, {l}]')
-                        # kernel pixel displacement from kernel origin
-                        dy = k - kernel.origin[0]
-                        dx = l - kernel.origin[1]
-                        # print(f'dy, dx [{dy}, {dx}]')
-                        # print(i + dy)
-                        # print(j + dx)
-                        # print(kernel.pixel(k, l) == 0)
-                        if(kernel.pixel(k, l) == 1): 
-                            if( (i + dy < 0) or (i + dy >= row) or (j + dx < 0) or (j + dx >= col) ): # if kernel pixel is out of image bound
-                                # print('out of bound')
-                                fit = False
-                                break
-                            if(image[i+dy, j+dx] == 0): # if kernel pixel has value but corresponding image pixel does not
-                                # print('value 0')
-                                fit = False
-                                break
-                        l += 1
-                        # print(f'fit : {fit}')
+            # scan each pixel in kernel
+            fit = True
+            for k in range(0, kernel.row):
+                if(not fit):    
+                    break
+                for l in range(kernel.col):
+                    # kernel pixel displacement from kernel origin
+                    dy = k - kernel.origin[0]
+                    dx = l - kernel.origin[1]
+                    if(kernel.pixel(k, l) == 1): 
+                        if( (i + dy < 0) or (i + dy >= row) or (j + dx < 0) or (j + dx >= col) ): # if kernel pixel is out of image bound
+                            result[i,j] = image[i,j]
+                            fit = False
+                            break
+                        if(image[i+dy, j+dx] == 0): # if kernel pixel has value but corresponding image pixel does not
+                            fit = False
+                            break
 
-                    k += 1
-                
-                if(fit):
-                    # preserve origin in result image
-                    result[i, j] = 1
+            # if the entire kernel fits into the image
+            if(fit):
+                # preserve origin in result image
+                result[i, j] = 1
     return result
 
-                        
-                        
+def binary_complement(image):
+    result = np.zeros(image.shape)
+    row, col = image.shape
+    for i in range(row):
+        for j in range(col):
+            result[i, j] = 1 - image[i, j]
+    
+    return result
+
+def image_intersection(image_A, image_B):
+    result = np.zeros(image_A.shape)
+    row, col = image.shape
+    for i in range(row):
+        for j in range(col):
+            if(image_A[i, j] == 1 and image_B[i, j] == 1):
+                result[i, j] = 1
+    
+    return result
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--source", default="lena.bmp")
+    parser.add_argument("-b", "--binary_threshold", default=128)
+    args = parser.parse_args()
+
     # read source image
-    image = cv.imread("lena.bmp", 0) # read with grayscale mode
+    image = cv.imread(args.source, 0) # read with grayscale mode
     # binarize
-    binary_image = binarize(image, threshold=128)
+    binary_image = binarize(image, threshold=int(args.binary_threshold))
 
     # setup octogonal kernel (3, 5, 5, 5, 3)
     oct_kernel_arr = np.array([ [0, 1, 1, 1, 0],
@@ -116,7 +127,7 @@ if __name__ == "__main__":
     L_kernel_J = Kernel(L_kernel_J_arr, (1, 1))
     L_kernel_K = Kernel(L_kernel_K_arr, (1, 1))
 
-    # # binary dilation
+    # binary dilation
     dilation_image = binary_dilation(binary_image, oct_kernel)
     plt.imsave('dilation.bmp', dilation_image, cmap='gray')
 
@@ -131,3 +142,8 @@ if __name__ == "__main__":
     # closing 
     closing_image = binary_erosion(dilation_image, oct_kernel)
     plt.imsave('closing.bmp', closing_image, cmap='gray')
+
+    # hit-and-miss
+    complement = binary_complement(binary_image)
+    hit_and_miss_image = image_intersection(binary_erosion(binary_image, L_kernel_J), binary_erosion(complement, L_kernel_K))
+    plt.imsave('hit_and_miss.bmp', hit_and_miss_image, cmap='gray')
